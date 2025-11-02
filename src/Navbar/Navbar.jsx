0046fd9174d436a62b38b4f60c24b03e784bbe51
sahab-mujaddid <sahab.mujaddid@gmail.com>
2025-11-02 08:13:51 +0600

    navbar,footer

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const  Navbar =()=>{
+    return(
+<div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+   
+    <a className="btn btn-ghost text-xl">TravelEase</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>Destination</a></li>
+     
+    </ul>
+  </div>
+  <div className="navbar-end">
+     <a className="btn">Login</a>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Root/Root.jsx';
+import Home from './Home/Home.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+        {
+  index:true,
+  path:"/",
+  element:<Home></Home>
+},
+
+  
+]
+  }
+ 
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router} />,
   </StrictMode>,
 )
